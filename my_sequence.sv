435ff7ee48ff43c087393d637f2a530c5d218766
thinhngo11 <35941824+thinhngo11@users.noreply.github.com>
2023-07-27 13:38:57 -0500

    Update my_sequence.sv

--- my_sequence.sv
+++ my_sequence.sv
@@ -1,26 +1,16 @@
-class my_item extends uvm_sequence_item;
-  `uvm_object_utils(my_item)
-  rand logic reset;
-  rand logic enable;
-  rand logic d;
-  logic q;
-  rand int delay;
-  
-  constraint c_reset {reset dist {1:=1, 0:=5};}  
-  constraint c_delay {delay < 3;}
+class my_sequence extends uvm_sequence #(my_item);
+  `uvm_object_utils(my_sequence)
   
   function new(string name="");
     super.new(name);
   endfunction
   
-  virtual function void do_copy(uvm_object rhs);
-    my_item tx;
-    $cast(tx, rhs);
-    super.do_copy(rhs);
-    reset = tx.reset;
-    enable = tx.enable;
-    d = tx.d;
-    q = tx.q;
-    delay = tx.delay;
-  endfunction
+  virtual task body();
+    req = my_item::type_id::create("m_item");
+    repeat (50) begin
+      if (!req.randomize()) `uvm_error("MYERR", "Can't randomize req");
+      start_item(req);
+      finish_item(req);
+    end
+  endtask
 endclass
